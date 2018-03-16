8e54229959991ee8111bab3afe81f2b5e20aef7d
Volodymyr Andriychak <andriychak@users.noreply.github.com>
2018-03-16 20:11:04 +0200

    initial commit

---
+++ src/app/employee.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { IEmployee } from './employee';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/throw'
+
+@Injectable()
+export class EmployeeService {
+
+  errorHandler(error: HttpErrorResponse) {
+    return Observable.throw(error.message || "Server Error");
+  }
+  private employeesUrl = 'assets/data/employees.json';
+
+  constructor(private http: HttpClient) { }
+
+  getEmployees():Observable<IEmployee[]> {
+    return this.http.get<IEmployee[]>(this.employeesUrl).catch(this.errorHandler);
+  }
+
+}
